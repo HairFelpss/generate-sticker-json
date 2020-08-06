9dae65f387c8115e2c59134e634220f1a067426f
Felipe Alves <felipe@iMac-de-Felipe.local>
2020-08-06 17:27:10 -0300

    set limit and search

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ require("dotenv/config");
 const express = require("express");
 const Packs = require("./models").packs;
 const Stickers = require("./models").stickers;
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
 const app = express();
 app.use(express.json());
 const port = 3333;
 
-app.get("/", async (req, res) => {
+app.get("/stickers/:count", async (req, res) => {
   try {
+    const { count } = req.params;
     const response = await Packs.findAndCountAll({
       attributes: [
         "id_pack",
@@ -23,8 +26,37 @@ app.get("/", async (req, res) => {
         {
           model: Stickers,
           as: "sticker",
-          limit: 10,
-          offset: 0,
+          attributes: ["image_name"],
+        },
+      ],
+      offset: count,
+      limit: 10,
+    });
+    res.json(response.rows);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+app.get("stickers/search/:search", async (req, res) => {
+  try {
+    const { search } = req.params;
+    const response = await Packs.findAndCountAll({
+      where: {
+        name: { [Op.like]: `%${search}%` },
+      },
+      attributes: [
+        "id_pack",
+        ["id_pack", "identifier"],
+        "name",
+        "url_zip",
+        "publisher",
+        ["url_base", "referencia"],
+      ],
+      include: [
+        {
+          model: Stickers,
+          as: "sticker",
           attributes: ["image_name"],
         },
       ],
