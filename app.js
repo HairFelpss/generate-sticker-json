2f0ca2d98cb976937bbba7948ca18b8c3ddebc58
Felipe Alves <felipe@iMac-de-Felipe.local>
2020-08-21 20:20:31 -0300

    fix json attribut name

--- app.js
+++ app.js
@@ -118,7 +118,7 @@ app.get("/savePack/:pack", async (req, res) => {
     });
 
     const webp = response.map((image) => ({
-      image_name: image.image_file.replace(".png", ".webp"),
+      image_file: image.image_name.replace(".png", ".webp"),
     }));
 
     res.json(webp);
